c4d217ad71cf515d6ce2cf7d62e5241774f19f22
Alex Sepel <35877463+Asopy@users.noreply.github.com>
2025-05-03 16:09:54 +0100

    Combined Simons assets with My assets

--- Scripts/camera_animation.gd
+++ Scripts/camera_animation.gd
@@ -3,7 +3,7 @@ extends Node3D
 @onready var pivot: Node3D = $Pivot
 
 # Rotation speed in radians per second
-@export var rotation_speed: float = 0.5
+@export var rotation_speed: float = -0.5
 
 func _process(delta: float) -> void:
 	pivot.rotate_y(rotation_speed * delta)
