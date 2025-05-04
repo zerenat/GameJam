615032aa086d039e9196dedd33c95a4ff665c521
Martin Hein <Sleight1@live.com>
2025-05-04 12:57:42 +0100

    Synced work from each branch

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,45 +1,57 @@
-class_name Enemy extends Node3D
+#class_name Enemy extends Node3D
+extends CharacterBody3D
 
-@export var health: float = 100.0
-@export var speed: float = 2.0
-@export var target: NodePath
+@export var health: int = 100
+#@export var speed: float = 2.0
+#@export var target: NodePath
+#
+#var target_node: Node3D
+#@onready var camera = get_node("../../Camera3D")
+#@onready var health_bar = get_node("../HealthBar")
+#
 
-var target_node: Node3D
-@onready var camera = get_node("../../Camera3D")
-@onready var health_bar = get_node("../HealthBar")
+#
+#func _ready():
+	#if has_node(target):
+		#target_node = get_node(target)
+#
+#func _process(delta):
+	#if target_node:
+		#print(health_bar)
+		#var direction = (target_node.global_position - global_position).normalized()
+		#global_position += direction * speed * delta
+		#var damage: float = 50 * delta
+		#take_damage(damage)
+#
+#func take_damage(damage: float):
+	#health -= damage
+	#if health <= 0:
+		#health = 0
+		#destroy()
+		#print("ded")
+	#display_health()
+		#
+#func display_health():
+	#if health:
+		#health = health  # Update it with current health
+	#print("Health left: " + str(health))
+	#
+@onready var path_follow_3d: PathFollow3D = $".."
 
-func _ready():
-	if has_node(target):
-		target_node = get_node(target)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _process(delta):
-	if target_node:
-		print(health_bar)
-		var direction = (target_node.global_position - global_position).normalized()
-		global_position += direction * speed * delta
-		var damage: float = 50 * delta
-		take_damage(damage)
 
-func take_damage(damage: float):
-	health -= damage
-	if health <= 0:
-		health = 0
-		destroy()
-		print("ded")
-	display_health()
-		
-func display_health():
-	if health:
-		health = health  # Update it with current health
-	print("Health left: " + str(health))
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	const move_speed := 0.05
+	path_follow_3d.progress += move_speed + delta
+
 	
 	
 func destroy():
 	queue_free()
-
-# extends CharacterBody3D
-
-var health = 100 # This is the health variable of the enemy
 
 func _on_tower_enemy_takes_damage(enemy: Variant, damage_amount: Variant) -> void:
 	print('---------------')
@@ -48,5 +60,5 @@ func _on_tower_enemy_takes_damage(enemy: Variant, damage_amount: Variant) -> voi
 		health = health - damage_amount
 		print(health)
 	elif health <= 0:
-			print("DEAD!")
-
+		print("DEAD!")
+		destroy()
